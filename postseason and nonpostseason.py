d35e37a5dc5d66044325976aea323d57e8ff07a6
Allen-Ho-0302 <allenh0302@gmail.com>
2020-07-15 21:14:42 -0400

    Logistic regression

--- postseason and nonpostseason.py
+++ postseason and nonpostseason.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import pyodbc
 
+#import regular season stats from MLB teams who got into postseason during 2012-2019
+#items include Tm, BatAge, PA, AB, R, H, 2B, 3B, HR, RBI, SB, CS, BB, SO, BA, OBP, SLG, OPS, TB, GDP
+#total rows are 8(years)*10(teams each year)=80
+
 sql_conn = pyodbc.connect('''DRIVER={ODBC Driver 13 for SQL Server};
                             SERVER=ALLENHO\MSSQLSERVER002;
                             DATABASE=Playoffbound;
@@ -39,8 +43,18 @@ from [dbo].['12B$']
 where Tm in ('TEX','BAL','OAK','NYY','DET','ATL','STL','SFG','WSN','CIN')
 '''
 df = pd.read_sql(query, sql_conn)
-print(df.head(3))
 
+#stored as df_post
+df_post = df
+
+#import regular season stats from MLB teams who DIDN'T get into postseason during 2012-2019
+#items are the same as above
+#total rows are 8(years)*20(teams each year)=160
+sql_conn = pyodbc.connect('''DRIVER={ODBC Driver 13 for SQL Server};
+                            SERVER=ALLENHO\MSSQLSERVER002;
+                            DATABASE=Playoffbound;
+                            Trusted_Connection=yes''') 
+query = '''
 select Tm, BatAge, PA, AB, R, H, 2B, 3B, HR, RBI, SB, CS, BB, SO, BA, OBP, SLG, OPS, TB, GDP
 from [dbo].['19B$']
 where Tm is not null and Tm not in ('WSN','LAD','MIL','ATL','STL','HOU','NYY','MIN','TBR','OAK', 'LgAvg')
@@ -72,4 +86,124 @@ UNION ALL
 select Tm, BatAge, PA, AB, R, H, 2B, 3B, HR, RBI, SB, CS, BB, SO, BA, OBP, SLG, OPS, TB, GDP
 from [dbo].['12B$']
 where Tm is not null and Tm not in ('TEX','BAL','OAK','NYY','DET','ATL','STL','SFG','WSN','CIN', 'LgAvg')
+'''
+df = pd.read_sql(query, sql_conn)
+
+#stored as df_npost
+df_npost = df
+
+#add each dataframe a new column named POST, which imply whether the team made the postseason
+df_post['POST']= True
+df_npost['POST']= False
+
+#append two dataframes together
+df_com=df_post.append(df_npost)
+
+#-----using logistic model to see team season OBP vs making the postseason that year or not-------------------------------
+
+# Load libraries and functions
+import statsmodels.api as sm
+from statsmodels.formula.api import glm
+import numpy as np
+
+# Define the formula of the logistic model to see how well team regular season OBP predict postseason birth
+model_formula = 'POST ~ OBP'
+# Define the correct probability distribution and the link function of the response variable
+link_function = sm.families.links.logit
+model_family = sm.families.Binomial(link = link_function)
+
+# Fit the model
+OBP_fit = glm(formula = model_formula, 
+                data = df_com, 
+                family = model_family).fit()
+# View the results of the OBP_fit model
+print(OBP_fit.summary())
+
+# Extract coefficients from the fitted model OBP_fit
+intercept, slope = OBP_fit.params
+
+# Print coefficients
+print('Intercept =', intercept)
+print('Slope =', slope)
+
+# Extract and print confidence intervals
+print(OBP_fit.conf_int())
+
+# Compute the multiplicative effect on the odds
+print('Odds: \n', np.exp(OBP_fit.params))
+
+# Define x at 1.5
+x = 0.315
+
+# Compute and print the estimated probability
+est_prob = np.exp(intercept + slope*x)/(1+np.exp(intercept + slope*x))
+print('Estimated probability at x = 0.315: ', round(est_prob, 4))
+
+# Compute the slope of the tangent line for parameter beta at x
+slope_tan = slope * est_prob * (1 - est_prob)
+print('The rate of change in probability: ', round(slope_tan,4))
+
+# Estimated covariance matrix: OBP_cov
+OBP_cov = OBP_fit.cov_params()
+print(OBP_cov)
+
+# Compute standard error (SE): std_error
+std_error = np.sqrt(OBP_cov.loc['OBP', 'OBP'])
+print('SE: ', round(std_error, 4))
+
+# Compute Wald statistic
+wald_stat = slope/std_error
+print('Wald statistic: ', round(wald_stat,4))
+
+# Extract and print confidence intervals
+print(OBP_fit.conf_int())
+
+# Compute confidence intervals for the odds
+print(np.exp(OBP_fit.conf_int()))
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Plot OBP and POST and add overlay with the logistic fit
+sns.regplot(x = 'OBP', y = 'POST', 
+            y_jitter=0.03,
+            data = df_com, 
+            logistic = True,
+            ci = None)
+# Display the plot
+plt.show()
+
+
+#-----assume there is now a new 2020 dataset named OBP_test, we can test how well it fits out model
+
+# Compute predictions for the test sample OBP_test and save as prediction
+prediction = OBP_fit.predict(exog = OBP_test)
+
+# Add prediction to the existing data frame OBP_test and assign column name prediction
+OBP_test['prediction'] = prediction
+
+# Examine the first 5 computed predictions
+print(OBP_test[['POST', 'OBP', 'prediction']].head())
+
+# Define the cutoff
+cutoff = 0.5
+
+# Compute class predictions: y_prediction
+y_prediction = np.where(prediction > cutoff, 1, 0)
+
+# Assign actual class labels from the test sample to y_actual
+y_actual = OBP_test['POST']
+
+# Compute the confusion matrix using crosstab function
+conf_mat = pd.crosstab(y_actual, y_prediction,
+					   rownames=['Actual'], 
+                  	   colnames=['Predicted'], 
+                       margins = True)
+
+# Print the confusion matrix
+print(conf_mat)
+
+#-----above code are examples of team season OBP vs making the postseason that year or not
+#-----the code can all be used to see the logistic model between team season stats vs teams making the postseason or not 
+
 
